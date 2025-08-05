969f802fe6c4e2892dbf72cc4aae74de51581d19
Andrea Cupertino <andreacupertino5@email.com>
2025-08-05 17:29:42 +0200

    Predisposizione per logica di gestione utenti

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//Import del controller di user
+//const userController = require('../controllers/userController');
+
+//// Rotta POST /api/user/login → login utente
+// router.post('/login',userController.loginUtente);
+//
+// // Rotta POST /api/user/register → crea un nuovo utente
+// router.post('/register', userController.creaUtente);
