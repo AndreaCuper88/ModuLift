2675dcd8b58d0e7f9b4742bf888217780615eb2e
Andrea Cupertino <andreacupertino5@email.com>
2025-08-05 15:32:54 +0200

    Setup cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN;
 
+//Middlewares
+app.use(cors({
+        origin: FRONTEND_ORIGIN,
+        credentials: true,
+    }
+))
 app.use(express.json());
 
 app.get('/', (req, res) => {
