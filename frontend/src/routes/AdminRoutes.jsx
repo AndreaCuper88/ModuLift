21c5ac1b51ee7e052b857dc2e96a65f999a8d3c0
Andrea Cupertino <andreacupertino5@email.com>
2025-08-10 16:44:49 +0200

    Aggiunta funzione AdminGuard per evitare che utenti non admin entrino in rotte protette

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -1,19 +1,49 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import DashboardAdmin from "../pages/adminPages/Dashboard";
 import GestioneClienti from "../pages/adminPages/GestioneClienti";
 import GestioneSchedaUtente from "../pages/adminPages/GestioneSchedaUtente";
 import GestionePianoUtente from "../pages/adminPages/GestionePianoUtente";
 import GestioneMisureCliente from "../pages/adminPages/GestioneMisureCliente";
 
+import useAuth from "../hooks/useAuth";
+
+function AdminGuard({children}) {
+    const {auth} = useAuth();
+    if (!auth?.user) return <Navigate to="/" replace/>
+    if(auth.user.ruolo !== "admin"){
+        return <Navigate to="/" replace/>
+    }
+    return children;
+}
+
 export default function AdminRoutes() {
     return (
         <Routes>
-            <Route path="/admin/dashboard" element={<DashboardAdmin />} />
-            <Route path="/admin/clienti" element={<GestioneClienti />} />
-            <Route path="/admin/scheda/:id" element={<GestioneSchedaUtente />} />
-            <Route path="/admin/piano/:id" element={<GestionePianoUtente />} />
-            <Route path="/admin/misure/:id" element={<GestioneMisureCliente />} />
-
+            <Route path="/admin/dashboard" element={
+                <AdminGuard>
+                    <DashboardAdmin />
+                </AdminGuard>
+            } />
+            <Route path="/admin/clienti" element={
+                <AdminGuard>
+                    <GestioneClienti />
+                </AdminGuard>
+            } />
+            <Route path="/admin/scheda/:id" element={
+                <AdminGuard>
+                    <GestioneSchedaUtente />
+                </AdminGuard>
+            } />
+            <Route path="/admin/piano/:id" element={
+                <AdminGuard>
+                    <GestionePianoUtente />
+                </AdminGuard>
+            } />
+            <Route path="/admin/misure/:id" element={
+                <AdminGuard>
+                    <GestioneMisureCliente />
+                </AdminGuard>
+            } />
         </Routes>
     );
 }
