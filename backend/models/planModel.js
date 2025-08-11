2e66e61c95d6b6ba5483f59bc85c0a83ae31faac
Andrea Cupertino <andreacupertino5@email.com>
2025-08-11 16:31:27 +0200

    Risoluzione problema nel salvataggio della Mappa chiave valore, non riuscivo a salvare i dati interni

--- backend/models/planModel.js
+++ backend/models/planModel.js
@@ -11,24 +11,7 @@ const planModel = new mongoose.Schema({
         required: true,
         min: 1
     },
-    plan: {
-        type: Map,  //Mappa chiave-valore, indico i tipi dei valori che saranno assegnati ad ogni chiave
-        of: [{
-            uid: { type: String, required: true },
-            exerciseId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Exercise',
-                required: true
-            },
-            scheme: [{
-                count: { type: Number, required: true },
-                reps: { type: Number, required: true }
-            }],
-            rest: { type: Number, default: 90 },
-            notes: { type: String, trim: true }
-        }],
-        default: {}
-    }
+    plan: { type: mongoose.Schema.Types.Mixed, default: {} } //Per risolvere errori sulla mappa chave valore, non aggiornava i valori interni
 }, { timestamps: true }); //Aggiungo automaticamente createdAt e updatedAt
 
 module.exports = mongoose.model('WorkoutPlan', planModel);
